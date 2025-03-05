1b006cc722e64f2204032a523a36e53c859893c5
Neranji Abeysekara <155614380+NeranjiAbeysekara@users.noreply.github.com>
2025-03-05 13:27:03 +0530

    home page components

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom' 
+import ShopContextProvider from './context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+  <ShopContextProvider>
+  <App />
+  </ShopContextProvider>
+
   </BrowserRouter>,
 )
