d33ef30361b69db5bcb042b0633becdd74aea32e
Neranji Abeysekara <155614380+NeranjiAbeysekara@users.noreply.github.com>
2025-03-06 14:49:23 +0530

    models and reg Api

---
+++ back-end/controllers/userController.js
@@ -0,0 +1,18 @@
+
+// Route for user login
+const loginUser =  async(req,res) =>{
+
+}
+// Route for user Register
+const registerUser = async (req,res) =>{
+
+res.json({msg: "Register API working"})
+
+}
+
+// route for admin login
+const adminLogin  = async (req,res) =>{
+ 
+}
+
+export {loginUser,registerUser, adminLogin}
--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 //app config
 
 const app = express()
@@ -14,7 +15,9 @@ app.use(express.json())
 app.use(cors())
 
 //api endpoints
+app.use('/api/user', userRouter)
 
+app.use('/api/user',userRouter)
 app.get('/',(req,res)=>{
     res.send ("API Working")
 })
