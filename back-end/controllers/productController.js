0d863e70ff0806b866a5f68ca320f0b9569f965b
Neranji Abeysekara <155614380+NeranjiAbeysekara@users.noreply.github.com>
2025-03-11 17:58:10 +0530

    saving to cloudinary

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -1,5 +1,5 @@
-
-
+import {v2 as cloudinary} from "cloudinary"
+import productModel from "../models/productModel.js" 
 //function for add product
 
 
@@ -10,22 +10,38 @@ const addProduct = async (req, res) => {
         const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
 
         // Safely access files, ensure they exist
-        const image1 =  req.files.image1[0];
-        const image2 =  req.files.image2[0];
-        const image3 =  req.files.image3[0] ;
-        const image4 =  req.files.image4[0] ;
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0] ;
+        const image4 = req.files.image4 && req.files.image4[0] ;
 
-        // Check if the required images are present
-        if (!image1 || !image2 || !image3 || !image4) {
-            return res.status(400).json({
-                success: false,
-                message: "All image fields are required."
-            });
-        }
+        const images = [image1, image2, image3, image4].filter((item)=>item !== undefined)
 
-        console.log(name, description, price, category, subCategory, sizes, bestseller);
-        console.log(image1, image2, image3, image4);
+        let imagesUrl = await Promise.all(
+            images.map(async (item)=>{
+                let result = await cloudinary.uploader.upload(item.path,{resource_type:'image'});
+                return result.secure_url
+            
+            })
+        )
 
+         const productData = {
+            name,
+            description,
+            category,
+            price:Number(price),
+            subCategory,
+            bestseller: bestseller === "true" ? true: false,
+            sizes:JSON.parse(sizes),
+            image:imagesUrl,
+            date: Date.now()
+         }
+
+         console.log(productData);
+         const product = new productModel(productData);
+         await product.save()
+
+         
         res.json({
             success: true,
             message: "Product added successfully"
