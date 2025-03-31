8f2af0757fe0782e3e8ffba315f90a42311f6a23
Neranji Abeysekara <155614380+NeranjiAbeysekara@users.noreply.github.com>
2025-03-31 12:17:56 +0530

    best seller and our policy front end

--- front-end/src/components/LatestCollection.jsx
+++ front-end/src/components/LatestCollection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
 
@@ -17,10 +18,15 @@ const LatestCollection = () => {
         <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique explicabo architecto, laborum aperiam doloremque ut harum eveniet, eum earum voluptate recusandae aliquid sunt itaque quod quaerat adipisci. Dolorum, debitis a.</p>
       </div>
-        
+         {/*rendering products */}
+         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+             {
+              latestProducts.map((item,index)=>(
+              <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+              ))
+             }
+         </div>
       
-
-
     </div>
   )
 }
--- front-end/src/components/ProductItem.jsx
+++ front-end/src/components/ProductItem.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import {Link} from 'react-router-dom'
+
 const ProductItem = ({id,image,name,price}) => {
 
     const {currency} = useContext(ShopContext);
@@ -10,12 +11,13 @@ const ProductItem = ({id,image,name,price}) => {
     <div className='overflow-hidden'>
        <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
     </div>
-    <p>{}</p>
+    <p className='pt-3 pb-1 text-sm'>{name}</p>
+    <p className='text-sm font-medium'>{currency}{price}</p>
     </Link>
   )
 }
 
 export default ProductItem
-{id,image,name,price}
+
 
 
